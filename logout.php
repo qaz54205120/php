421502c74e0cd38482bc2157a54c08d85b59fae0
qaz54205120 <48427622+qaz54205120@users.noreply.github.com>
2021-05-25 18:08:32 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+//啟動 session_start();
+session_start();
+
+//把該使用者 session 銷毀，登入資訊就不見了
+session_destroy();
+$str = $_SERVER['QUERY_STRING'];
+
+//轉跳回後台首頁，因為此 logout 是放在 php 資料夾內，若要前往 admin，就要回上一層 ../ 找到 admin 才能進入 index.php
+header("Location: ../login.php?$str ");
+
+?>
